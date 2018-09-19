bdaeca74a3ebd5cdd21dba54ea2868835f180c20
dattajadhav44 <40449078+dattajadhav44@users.noreply.github.com>
2018-09-19 23:22:16 +0530

    Add files via upload

---
+++ 05_blank-identifier/01_invalid-code/main.go
@@ -0,0 +1,11 @@
+
+package main 
+
+import "fmt"
+
+func main() {
+	a := "Stored value in var a"
+	b := "Stored value in var b"
+	fmt.Println("Here is a result -", a)
+	//fmt.Println("b", b) The program will not compile because the b is declare but not used.
+}
